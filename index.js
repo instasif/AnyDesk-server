8f7c1b396da1a97cdf2428e17e0769a853a0336a
instasif <asifh9277@gmail.com>
2022-11-28 17:18:58 +0600

    admin route problem solved

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ async function run(){
 
         /**sakjdhkasjfhksdjhfkdsjhfkj */
         app.get('/users/admin/:email', async(req, res)=>{
-            const email = req.params.eamil;
+            const email = req.params.email;
             const query = { email};
             const user = await usersCollection.findOne(query);
             res.send({isAdmin: user?.role === 'admin'});
